1ae97d00321e42374b7bf642f56edc82142760d0
AffanHossainRakib <affanhossainrakib+github@gmail.com>
2024-02-03 01:53:05 +0600

    bubbleSort added

---
+++ bubbleSort.py
@@ -0,0 +1,16 @@
+def bubbleSort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+        print(arr)
+
+def optimizedBubbleSort(arr):
+    for i in range(len(arr) - 1):
+        flag = True
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                flag = False
+        if flag:
+            break
