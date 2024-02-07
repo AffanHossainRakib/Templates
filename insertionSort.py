90079f00bb1a9fc199927e6f4598677aa0b3ec03
AffanHossainRakib <affanhossainrakib+github@gmail.com>
2024-02-07 12:22:11 +0600

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -1,11 +1,11 @@
-def insertionSort(n, arr):
-    for i in range(n - 1):
+def insertionSort(n, arr) -> list:  # Length of the array and the Array
+    for i in range(1, n, 1): # (i = 1; i < n; i++)
         key = arr[i]
         j = i - 1
         
-        while j >= 0 and key < arr[j]:
+        while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
-            j -= 1
+            j -= 1           
         arr[j + 1] = key
 
     return arr
