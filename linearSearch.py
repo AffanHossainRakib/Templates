d678b71007d50851015ccaef50013f83801fc17e
AffanHossainRakib <affanhossainrakib+github@gmail.com>
2024-02-13 13:02:01 +0600

    Update linearSearch.py

--- linearSearch.py
+++ linearSearch.py
@@ -1,15 +1,15 @@
-def linSearch(arr, key) -> int: # iterative
+def LinSearch(arr, key) -> int: # iterative
     for idx in range(len(arr)):
         if arr[idx] == key:
             return idx
     return -1
 
-def recLinSearch(arr, key, idx=0) -> int: # recursive
+def RecLinSearch(arr, key, idx=0) -> int: # recursive
     if idx >= len(arr):
         return -1
     if arr[idx] == key:
         return idx
-    return recLinSearch(arr, key, idx+1)
+    return RecLinSearch(arr, key, idx+1)
 
 # arr = [1, 2, 4, 5, 7, 8, 20, 20, 221, 222, 230]
 # print(linSearch(arr, 500))
