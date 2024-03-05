8a1e6c513f79e35fa56ca21e6f7dab1c18c28d2c
AffanHossainRakib <affanhossainrakib+github@gmail.com>
2024-03-05 19:34:49 +0600

    Update karatsuba.py

--- karatsuba.py
+++ karatsuba.py
@@ -1,15 +1,16 @@
 def karatsuba(x, y):
-    print(x,y)
-    if x < 10 and y < 10:
+    if len(str(x)) == 1 and len(str(y)) == 1:
         return x*y
     
     n = max(len(str(x)), len(str(y)))
     m = n // 2
     p = 10 ** m
 
+    # xh, xl = divmod(x, p)
     xh = x // p
     xl = x % p
 
+    # yh, yl = divmod(y, p)
     yh = y // p
     yl = y % p 
 
@@ -20,11 +21,6 @@ def karatsuba(x, y):
     return first*(10**(2*m)) + mid*(10**m) + last
 
 
-# arr = [(5,6), (100,343), (1234,5678), (111221243, 938)]
-# for a,b in arr:
-#     print(karatsuba(a,b) == a*b)
-
-# print(100*343)
-# print(karatsuba(100,343))
-
-print(karatsuba(1234, 5678))
+arr = [(5,6), (100,343), (1234,5678), (111221243, 938)]
+for a,b in arr:
+    print(karatsuba(a,b) == a*b)
